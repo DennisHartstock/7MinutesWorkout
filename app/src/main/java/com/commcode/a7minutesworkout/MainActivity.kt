ff840fca5d26c3c112c0a516c23a65164b264fe3
DennsHartstock <dennis.hartstock@web.de>
2023-02-05 11:30:25 +0100

    set up UI

--- app/src/main/java/com/commcode/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/commcode/a7minutesworkout/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.commcode.a7minutesworkout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var flStart: FrameLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+
+        flStart.setOnClickListener {
+            Toast.makeText(this, "Start clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun initViews() {
+        flStart = findViewById(R.id.flStart)
     }
 }
