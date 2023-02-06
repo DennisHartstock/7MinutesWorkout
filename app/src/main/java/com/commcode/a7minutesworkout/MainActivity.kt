f47961aec0a5add702fd380da8e44a2db488b506
DennsHartstock <dennis.hartstock@web.de>
2023-02-06 08:27:23 +0100

    add viewBinding

--- app/src/main/java/com/commcode/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/commcode/a7minutesworkout/MainActivity.kt
@@ -1,24 +1,21 @@
 package com.commcode.a7minutesworkout
 
 import android.os.Bundle
-import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.commcode.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var flStart: FrameLayout
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        initViews()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        flStart.setOnClickListener {
+        binding.flStart.setOnClickListener {
             Toast.makeText(this, "Start clicked", Toast.LENGTH_SHORT).show()
         }
-    }
-
-    private fun initViews() {
-        flStart = findViewById(R.id.flStart)
     }
 }
